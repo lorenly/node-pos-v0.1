87eb72db1d0b879a6085b889659d177eae3e521c
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-07 20:31:13 +0800

    Updated

--- main/main.js
+++ main/main.js
@@ -1,4 +1,86 @@
-module.exports = function main() {
+module.exports = function main(inputs) {
     console.log("Debug Info");
-    return 'Hello World!';
+    return printReceipt(inputs);
 };
+
+const cola = 'ITEM000000';
+const sprite = 'ITEM000001';
+const battery = 'ITEM000004';
+
+function printReceipt(inputs){
+	let receipt = "";
+	let header = '***<store earning no money>Receipt ***\n';
+	let prodName;
+	let qty;
+	let unit;
+	let price;
+	let ttl;
+	let product = [];
+	let ttlYuan = 0;
+	receipt += header;
+
+	inputs.forEach(function(i){
+		price = 0;
+		prodName = i.Name;
+		price = i.Price;
+		switch(i.Barcode){
+			case cola:
+				if(!product.includes(cola)){
+					qty = getQty(inputs, cola);
+					ttl = qty * price;
+					product.push(cola);
+					if(qty > 1){
+						unit = ' ' + i.Unit + 's';
+					}else{
+						unit = ' ' + i.Unit;
+					}
+					receipt += getProduct(prodName, qty, unit, price.toFixed(2), ttl.toFixed(2));
+					ttlYuan += ttl;
+				}
+				break;
+			case sprite:
+				if(!product.includes(sprite)){
+					qty = getQty(inputs, sprite);
+					ttl = qty * price;
+					product.push(sprite);
+					if(qty > 1){
+						unit = ' ' + i.Unit + 's';
+					}else{
+						unit = ' ' + i.Unit;
+					}
+					receipt += getProduct(prodName, qty, unit, price.toFixed(2), ttl.toFixed(2));
+					ttlYuan += ttl;
+				}
+			case battery:
+				if(!product.includes(battery) && i.Name == 'Battery'){
+					qty = getQty(inputs, battery);
+					unit = '';
+					ttl = qty * price;
+					product.push(battery);
+					receipt += getProduct(prodName, qty, unit, price.toFixed(2), ttl.toFixed(2));
+					ttlYuan += ttl;
+				}
+		}
+	});
+	receipt += '----------------------\n' +
+            `Total: ${ttlYuan.toFixed(2)} (yuan)\n` +
+            '**********************\n';
+
+	
+	return receipt;
+}
+
+function getQty(prod, id){
+	let qty = 0;
+	return qty = prod.reduce((qty, cur) => cur.Barcode === id ? ++qty : qty, 0);
+}
+
+function getProduct(prodName, qty, unit, price, ttl){
+	return `Name: ${prodName}, Quantity: ${qty}${unit}, Unit price: ${price} (yuan), Subtotal: ${ttl} (yuan)\n`;
+}
+
+function addTotal(qty, val){
+	return qty + val;
+}
+
+
